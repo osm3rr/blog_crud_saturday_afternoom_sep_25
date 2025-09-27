601e6817143fcdaf2c1219208e957b3134b38b3f
osm3rr <osm3rr@adakademy.com>
2025-09-27 17:06:09 -0400

    create added

--- blogs/views.py
+++ blogs/views.py
@@ -1,14 +1,18 @@
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
-
-
 class PostListView(ListView):
     model = Post
     template_name = "post_list.html"
 
-
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ['title', 'body', 'author']
+
